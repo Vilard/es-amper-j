63b427e186b2ddcd019b900cf755a615d55131dd
Vladimir <vladimir052290@mail.ru>
2018-07-12 21:27:56 +0300

    seo

--- _site/js/menu.js
+++ _site/js/menu.js
@@ -1,11 +1,2 @@
-$(document).ready(function() {
-  $('.menu-icon').click(function() {
-    $('.trigger').slideToggle(500);
-  });//end slide toggle
-  
- //  $(window).resize(function() {		
-	// 	if (  $(window).width() > 800 ) {			
-	// 		$('.trigger').removeAttr('style');
-	// 	 }
-	});//end resize
-	// $('')
+$(document).ready(function(){$('.menu-icon').click(function(){$('.trigger').slideToggle(500)})});
+
